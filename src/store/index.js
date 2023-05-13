bb102c256667743b6612ea9f3664e055c80bb674
Eren Küçük <erenkucuk2002@gmail.com>
2023-05-13 23:58:21 +0300

    State manangement added

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    // Durumlarınızı burada tanımlayın
+  },
+  mutations: {
+    // Mutasyonlarınızı burada tanımlayın
+  },
+  actions: {
+    // Eylemlerinizi burada tanımlayın
+  },
+  getters: {
+    // Getters'ları burada tanımlayın
+  },
+});
